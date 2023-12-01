f98bc70ad54c38c9058a7ee22ee625710e4ce765
Alejandra Sanchez <alejitasanvar@gmail.com>
2023-12-01 17:54:48 -0500

    inicio recuperacion del front

--- Controladores/Controlador.Landing.php
+++ Controladores/Controlador.Landing.php
@@ -8,8 +8,9 @@
         public function main()
         {
 
-        	require_once("Vistas/Landing/Modulos/");
-        	
+        	require_once("Vistas/Landing/Modulos/Header.php");
+            require_once("Vistas/Landing/Paginas/Inicio.php");
+        	require_once("Vistas/Landing/Modulos/Footer.php");
 
         }
 
--- Vistas/Landing/Paginas/Inicio.php
+++ Vistas/Landing/Paginas/Inicio.php
@@ -1,5 +1,7 @@
-<div class="content">
-    <div class="animated fadeIn">
-
+<div class="container-fluid">
+    <div class="row">
+        <div class="col">
+            <h1 class="text-center">pagina de inicio campeche en el front</h1>
+        </div>
     </div>
 </div>
