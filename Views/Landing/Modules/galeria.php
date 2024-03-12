6c70f178ca540a20d48aef96162d5b5ab014f73f
Ronal Cucariano <ronalcucariano@gmail.com>
2024-03-12 01:25:09 -0500

    Se cambia el banner

--- Views/Landing/Modules/galeria.php
+++ Views/Landing/Modules/galeria.php
@@ -1,109 +1,38 @@
-<<<<<<< HEAD:Vistas/Landing/Modulos/galeria.php
 <style type="text/css">
     .imagen-carrusel {
-        height: 400px; /* Ajusta el valor según la altura deseada */  
+        height: 200px; /* Ajusta el valor según la altura deseada */  
 }
 </style>
-
-<div class="row">
-    <div class="col col-sm-2"></div>
-        <div class="col col-sm-8">
-                <div id="carouselExampleCaptions" class="carousel mx-auto slide">
-                  <div class="carousel-indicators">
-                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" aria-label="Slide 4"></button>
-                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="4" aria-label="Slide 5"></button>
-                  </div>
-                  <div class="carousel-inner justify-content-center">
-                    <div class="carousel-item active p-3 ">
-                      <img src="Vistas/Landing/Dependencias/imagenes/galeria/plaza6.jpg" class="d-block w-100 imagen-carrusel" alt="..." style="object-fit: contain;">
-                      <div class="carousel-caption d-none d-md-block">
-                        <h5>First slide label</h5>
-                        <p>Some representative placeholder content for the first slide.</p>
-                      </div>
-                    </div>
-                    <div class="carousel-item p-3">
-                      <img src="Vistas/Landing/Dependencias/imagenes/galeria/plaza2.png" class="d-block w-100 imagen-carrusel" alt="..." style="object-fit: contain;">
-                      <div class="carousel-caption d-none d-md-block">
-                        <h5>Second slide label</h5>
-                        <p>Some representative placeholder content for the second slide.</p>
-                      </div>
-                    </div>
-                    <div class="carousel-item p-3">
-                      <img src="Vistas/Landing/Dependencias/imagenes/galeria/plaza3.jpg" class="d-block w-100 imagen-carrusel" alt="..." style="object-fit: contain;">
-                      <div class="carousel-caption d-none d-md-block">
-                        <h5>Third slide label</h5>
-                        <p>Some representative placeholder content for the third slide.</p>
-                      </div>
-                    </div>
-                    <div class="carousel-item p-3">
-                      <img src="Vistas/Landing/Dependencias/imagenes/galeria/plaza7.jpg" class="d-block w-100 imagen-carrusel" alt="..." style="object-fit: contain;">
-                      <div class="carousel-caption d-none d-md-block">
-                        <h5>Third slide label</h5>
-                        <p>Some representative placeholder content for the third slide.</p>
-                      </div>
-                    </div>
-                    <div class="carousel-item p-3">
-                      <img src="Vistas/Landing/Dependencias/imagenes/galeria/plaza5.jpg" class="d-block w-100 imagen-carrusel" alt="..." style="object-fit: contain;">
-                      <div class="carousel-caption d-none d-md-block">
-                        <h5>Third slide label</h5>
-                        <p>Some representative placeholder content for the third slide.</p>
-                      </div>
-                    </div>
-                  </div>
-                  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Previous</span>
-                  </button>
-                  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Next</span>
-                  </button>
-                </div>
+<div class="col col-sm-2"></div>
+  <div class="col col-sm-12">
+    <div class="row">
+      <div id="carouselExampleRide" class="carousel slide" data-bs-ride="true">
+        <div class="carousel-inner">
+          <div class="carousel-item active" data-bs-interval="10000">
+            <img src="Views/Landing/Dependencias/imagenes/galeria/plaza6.jpg" class="d-block w-100" alt="...">
+          </div>
+          <div class="carousel-item" data-bs-interval="2000">
+            <img src="Views/Landing/Dependencias/imagenes/galeria/plaza2.png" class="d-block w-100" alt="...">
+          </div>
+          <div class="carousel-item" data-bs-interval="3000">
+            <img src="Views/Landing/Dependencias/imagenes/galeria/plaza3.jpg" class="d-block w-100" alt="...">
+          </div>
+          <div class="carousel-item" data-bs-interval="4000">
+            <img src="Views/Landing/Dependencias/imagenes/galeria/plaza7.jpg" class="d-block w-100" alt="...">
+          </div>
+          <div class="carousel-item">
+            <img src="Views/Landing/Dependencias/imagenes/galeria/plaza5.jpg" class="d-block w-100" alt="...">
+          </div>
         </div>
-    <div class="col col-sm-2"></div>
-=======
-<div class="container2">
-<section class="imagenesgaleria">
-    <div class="image active" style="background-image: url(Views/Landing/Dependencias/imagenes/galeria/plaza6.jpg);">
-        <div class="label">
-            <div class="icon">
-                <i class="bi bi-camera"></i>
-            </div>
-        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleRide" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleRide" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
     </div>
-    <div class="image" style="background-image: url(Views/Landing/Dependencias/imagenes/galeria/plaza2.png);">
-        <div class="label">
-            <div class="icon">
-                <i class="bi bi-camera"></i>
-            </div>
-        </div>
-    </div>
-    <div class="image" style="background-image: url(Views/Landing/Dependencias/imagenes/galeria/plaza3.jpg);">
-        <div class="label">
-            <div class="icon">
-                <i class="bi bi-camera"></i>
-            </div>
-        </div>
-    </div>
-    <div class="image" style="background-image: url(Views/Landing/Dependencias/imagenes/galeria/plaza7.jpg);">
-        <div class="label">
-            <div class="icon">
-                <i class="bi bi-camera"></i>
-            </div>
-        </div>
-    </div>
-    <div class="image" style="background-image: url(Views/Landing/Dependencias/imagenes/galeria/plaza5.jpg);">
-        <div class="label">
-            <div class="icon">
-                <i class="bi bi-camera"></i>
-            </div>
-        </div>
-    </div>
-    <br>
-    <!--CONTENEDOR 3-->
-</section>
->>>>>>> Ronalc:Views/Landing/Modules/galeria.php
+  </div>
 </div>
--- Views/Landing/Plantilla.php
+++ Views/Landing/Plantilla.php
@@ -6,26 +6,12 @@
     <title>Campeche.com</title>
     
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-<<<<<<< HEAD:Vistas/Landing/Plantilla.php
-
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    
-    <script src="Vistas/Dependencias/js/app.js"></script>
-    
-    <link rel="stylesheet" href="Vistas/Dependencias/css/reset.css">
-    <link rel="stylesheet" href="Vistas/Dependencias/css/stylecompras.css">
-    <link rel="stylesheet" href="Vistas/Dependencias/css/campeche.css">
-    <link rel="stylesheet" href="Vistas/Dependencias/css/fuentes.css">
-=======
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
     <script src="Views/Dependencias/js/app.js"></script>
     <link rel="stylesheet" href="Views/Dependencias/css/reset.css">
     <link rel="stylesheet" href="Views/Dependencias/css/stylecompras.css">
     <link rel="stylesheet" href="Views/Dependencias/css/campeche.css">
     <link rel="stylesheet" href="Views/Dependencias/css/fuentes.css">
->>>>>>> Ronalc:Views/Landing/Plantilla.php
     
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
   </head>
