bc022a431ec9ba5ca53b588b29204b38be8abf14
Ronal Cucariano <ronalcucariano@gmail.com>
2024-03-13 03:47:20 -0500

    se elimino carpeta duplicada

--- Vistas/Landing/Paginas/Inicio.php
+++
@@ -1,3 +0,0 @@
-  <?php require_once("Vistas/Landing/Modulos/galeria.php") ?>
-  <?php require_once("Vistas/Landing/Modulos/banner.php") ?>
-  <?php require_once("Vistas/Landing/Modulos/dato_curioso_form_reg.php") ?>
