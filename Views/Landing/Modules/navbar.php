7ea7f03e71395887c6b37c33256f83a12fc184ad
Jose bohorquez <bd567358546@gmail.com>
2024-03-14 20:42:10 -0500

    cambios del front

--- Views/Landing/Modules/navbar.php
+++ Views/Landing/Modules/navbar.php
@@ -1,39 +1,45 @@
-<div class="row">
-    
-    <!--<nav class="navbar navbar-expand-lg bg-body-tertiary" >-->
-    <nav class="navbar navbar-expand-lg rounded-bottom-5" style="background-color: #8FBC8F;">
-        <div class="container-fluid">
-            <a class="navbar-brand Titulo" href="#">
-                <p><strong>Campeche</strong></p>
+
+
+
+
+<nav class="navbar navbar-expand-lg rounded-bottom-5" style="background-color: #8FBC8F;">
+  <div class="container-fluid">
+    <a class="navbar-brand Titulo" href="#">
+    <p><strong>Campeche</strong></p>
             </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <!--<div class="dropdown" data-bs-theme="dark">
-                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButtonDark" data-bs-toggle="dropdown" aria-expanded="false">
-                Somos campeche
-                </button>
-                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButtonDark">
-                        <li><a class="dropdown-item active" href="#">Somos campeche</a></li>
-                        <li><a class="dropdown-item" href="#">Action</a></li>
-                        <li><a class="dropdown-item" href="#">Another action</a></li>
-                        <li><a class="dropdown-item" href="#">Something else here</a></li>
-                        <li><hr class="dropdown-divider"></li>
-                        <li><a class="dropdown-item" href="#">Separated link</a></li>
-                    </ul>
-                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButtonDark" data-bs-toggle="dropdown" aria-expanded="false">
-                Mi perfil
-                </button>
-                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButtonDark">
-                        <li><a class="dropdown-item active" href="#">Mi</a></li>
-                        <li><a class="dropdown-item" href="#">Action</a></li>
-                        <li><a class="dropdown-item" href="#">Another action</a></li>
-                        <li><a class="dropdown-item" href="#">Something else here</a></li>
-                        <li><hr class="dropdown-divider"></li>
-                        <li><a class="dropdown-item" href="#">Separated link</a></li>
-                    </ul>-->
+
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    
+    <div class="Texbanner collapse navbar-collapse" id="navbarNavDropdown">
+        <li class="nav-item">
+          <a class="nav-link" href="#">Inicio</a>
+        </li>
+      <ul class="navbar-nav">
+        <li class="nav-item">
+        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Somos Campeche
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown link
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+    <!--
                 </div>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
