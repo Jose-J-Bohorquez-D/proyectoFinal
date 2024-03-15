288febb97d546fb89eb9e950c25266a84a10783e
Jose bohorquez <bd567358546@gmail.com>
2024-03-14 21:18:57 -0500

    Se cambia el banner

--- Views/Landing/Modules/navbar.php
+++ Views/Landing/Modules/navbar.php
@@ -1,7 +1,7 @@
 
 
 
-
+<div>
 <nav class="navbar navbar-expand-lg rounded-bottom-5" style="background-color: #8FBC8F;">
   <div class="container-fluid">
     <a class="navbar-brand Titulo" href="#">
@@ -12,22 +12,24 @@
       <span class="navbar-toggler-icon"></span>
     </button>
     
-    <div class="Texbanner collapse navbar-collapse" id="navbarNavDropdown">
+    <div class=" collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="Texbanner navbar-nav">
         <li class="nav-item">
           <a class="nav-link" href="#">Inicio</a>
         </li>
-      <ul class="navbar-nav">
         <li class="nav-item">
         <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Somos Campeche
           </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
+        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Pedidos
+          </a>
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown link
+           Mi Perfil
           </a>
           <ul class="dropdown-menu">
             <li><a class="dropdown-item" href="#">Action</a></li>
@@ -36,28 +38,9 @@
           </ul>
         </li>
       </ul>
+      <button class="btn btn-outline-success" type="submit">Contacto</button>
     </div>
   </div>
 </nav>
-    <!--
-                </div>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link Texbanner" aria-current="page" href="#">Inicio</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link Texbanner" href="#">Somos Campeche</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link Texbanner" href="#">Tu Pedido</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link Texbanner" href="#">Mi Perfil</a>
-                    </li>
-                </ul>
-                <button class="btn btn-outline-success" type="submit">Contacto</button>
-            </div>
-        </div>
-    </nav>
 </div>
+    
