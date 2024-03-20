be4c341485100bd0f502627e559fdbc69ae45467
Jose bohorquez <bd567358546@gmail.com>
2024-03-20 12:57:45 -0500

    ajustando el tercer modulo con el formulario responsivo , falta terminar el form

--- app/Views/landing/paginas/Inicio2.php
+++ app/Views/landing/paginas/Inicio2.php
@@ -1,53 +1,75 @@
 <div class="container-sm" style="opacity: 0.9; ">
+  <!-- inicio carrusel -->
   <div class="row d-flex justify-content-center">
     <div class="col-12 col-xs-12 col-sm-12 col-md-12 col-lg-2 col-xl-2 col-xxl-1"></div>
     <div class="col-12 col-xs-12 col-sm-12 col-md-12 col-lg-8 col-xl-8 col-xxl-10">
       <div id="carouselExampleIndicators" class="carousel slide">
         <div class="carousel-indicators">
-          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
-          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="4" aria-label="Slide 5"></button>       
-          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="5" aria-label="Slide 6"></button>
-          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="6" aria-label="Slide 7"></button>
-          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="7" aria-label="Slide 8"></button>
-          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="8" aria-label="Slide 9"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"
+            aria-current="true" aria-label="Slide 1"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
+            aria-label="Slide 2"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
+            aria-label="Slide 3"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3"
+            aria-label="Slide 4"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="4"
+            aria-label="Slide 5"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="5"
+            aria-label="Slide 6"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="6"
+            aria-label="Slide 7"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="7"
+            aria-label="Slide 8"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="8"
+            aria-label="Slide 9"></button>
         </div>
         <div class="carousel-inner mx-auto p-5">
           <div class="carousel-item active">
-            <img src="../assets/dependencias/landing/dependencias/imagenes/webP/plaza6.webp" class="d-block w-100 rounded-5 img-fluid" alt="...">
+            <img src="../assets/dependencias/landing/dependencias/imagenes/webP/plaza6.webp"
+              class="d-block w-100 rounded-5 img-fluid" alt="...">
           </div>
           <div class="carousel-item">
-            <img src="../assets/dependencias/landing/dependencias/imagenes/webP/plaza2.webp" class="d-block w-100 rounded-5 img-fluid" alt="...">
+            <img src="../assets/dependencias/landing/dependencias/imagenes/webP/plaza2.webp"
+              class="d-block w-100 rounded-5 img-fluid" alt="...">
           </div>
           <div class="carousel-item">
-            <img src="../assets/dependencias/landing/dependencias/imagenes/webP/agricultura.webp" class="d-block w-100 rounded-5 img-fluid" alt="...">
+            <img src="../assets/dependencias/landing/dependencias/imagenes/webP/agricultura.webp"
+              class="d-block w-100 rounded-5 img-fluid" alt="...">
           </div>
           <div class="carousel-item">
-            <img src="../assets/dependencias/landing/dependencias/imagenes/webP/plaza7.webp" class="d-block w-100 rounded-5 img-fluid" alt="...">
+            <img src="../assets/dependencias/landing/dependencias/imagenes/webP/plaza7.webp"
+              class="d-block w-100 rounded-5 img-fluid" alt="...">
           </div>
           <div class="carousel-item">
-            <img src="../assets/dependencias/landing/dependencias/imagenes/webP/variedad.webp" class="d-block w-100 rounded-5 img-fluid" alt="...">
+            <img src="../assets/dependencias/landing/dependencias/imagenes/webP/variedad.webp"
+              class="d-block w-100 rounded-5 img-fluid" alt="...">
           </div>
           <div class="carousel-item">
-            <img src="../assets/dependencias/landing/dependencias/imagenes/webP/familia.webp" class="d-block w-100 rounded-5 img-fluid" alt="...">
+            <img src="../assets/dependencias/landing/dependencias/imagenes/webP/familia.webp"
+              class="d-block w-100 rounded-5 img-fluid" alt="...">
           </div>
           <div class="carousel-item">
-            <img src="../assets/dependencias/landing/dependencias/imagenes/webP/plaza3.webp" class="d-block w-100 rounded-5 img-fluid" alt="...">
+            <img src="../assets/dependencias/landing/dependencias/imagenes/webP/plaza3.webp"
+              class="d-block w-100 rounded-5 img-fluid" alt="...">
           </div>
           <div class="carousel-item">
-            <img src="../assets/dependencias/landing/dependencias/imagenes/webP/plaza5.webp" class="d-block w-100 rounded-5 img-fluid" alt="...">
+            <img src="../assets/dependencias/landing/dependencias/imagenes/webP/plaza5.webp"
+              class="d-block w-100 rounded-5 img-fluid" alt="...">
           </div>
           <div class="carousel-item">
-            <img src="../assets/dependencias/landing/dependencias/imagenes/webP/slider5.webp" class="d-block w-100 rounded-5 img-fluid" alt="...">
+            <img src="../assets/dependencias/landing/dependencias/imagenes/webP/slider5.webp"
+              class="d-block w-100 rounded-5 img-fluid" alt="...">
           </div>
         </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-          <span style="background-color:transparent; color: #8FBC8F" class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
+          data-bs-slide="prev">
+          <span style="background-color:transparent; color: #8FBC8F" class="carousel-control-prev-icon"
+            aria-hidden="true"></span>
           <span class="visually-hidden">Previous</span>
         </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"
+          data-bs-slide="next">
           <span class="carousel-control-next-icon" aria-hidden="true"></span>
           <span class="visually-hidden">Next</span>
         </button>
@@ -55,42 +77,113 @@
     </div>
     <div class="col-12 col-xs-12 col-sm-12 col-md-12 col-lg-2 col-xl-2 col-xxl-1"></div>
   </div>
-</div>
-
+</div><!-- fin modulo carrusel -->
 <div class="container-fluid">
+  <!-- inicio modulo medio categorias -->
   <div class="row">
-    <div class="col text-center"><br> <br>  
+    <div class="col text-center"><br> <br>
       <h3>CATEGORIAS</h3><br>
       <h4>trabajando con los CAMPESINOS</h4><br><br>
       <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-5">
         <div class="col">
-        <div class="card bg-transparent text-white">
-          <img src="../assets/dependencias/landing/dependencias/imagenes/webP/frutas.webp" class="rounded-5" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">Frutas</h5>
-            <p class="card-text"><button class="btn btn-success" type="button">Ver Mas</button></p>
+          <div class="card bg-transparent text-white">
+            <img src="../assets/dependencias/landing/dependencias/imagenes/webP/frutas.webp" class="rounded-5"
+              alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Frutas</h5>
+              <p class="card-text"><button class="btn btn-success" type="button">Ver Mas</button></p>
+            </div>
           </div>
         </div>
-      </div>
-      <div class="col">
-        <div class="card bg-transparent text-white">
-          <img src="../assets/dependencias/landing/dependencias/imagenes/webP/vegetales.webp" class="rounded-5" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">Vegetales</h5>
-            <p class="card-text"><button class="btn btn-success" type="button">Ver Mas</button></p>
+        <div class="col">
+          <div class="card bg-transparent text-white">
+            <img src="../assets/dependencias/landing/dependencias/imagenes/webP/vegetales.webp" class="rounded-5"
+              alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Vegetales</h5>
+              <p class="card-text"><button class="btn btn-success" type="button">Ver Mas</button></p>
+            </div>
           </div>
         </div>
-      </div>
-      <div class="col">
-        <div class="card bg-transparent text-white">
-          <img src="../assets/dependencias/landing/dependencias/imagenes/webP/hortalizas.webp" class="rounded-5" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">Hortalizas</h5>
-            <p class="card-text"><button class="btn btn-success" type="button">Ver Mas</button></p>
+        <div class="col">
+          <div class="card bg-transparent text-white">
+            <img src="../assets/dependencias/landing/dependencias/imagenes/webP/hortalizas.webp" class="rounded-5"
+              alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Hortalizas</h5>
+              <p class="card-text"><button class="btn btn-success" type="button">Ver Mas</button></p>
+            </div>
           </div>
         </div>
       </div>
     </div>
+  </div>
+</div><!-- fin modulo medio categorias -->
+<br><br>
+
+
+<div class="container-fluid">
+  <div class="row row-cols-1 d-flex justify-content-evenly align-items-start">
+    <div class="col-12 col-sm-12 col-md-8 ">
+      <div class="row">
+
+
+        <div class="card mb-3" style="max-width: auto;">
+          <div class="row g-0">
+            <div class="col-md-4">
+              <img src="../assets/dependencias/landing/dependencias/imagenes/webP/info1.webp" class="img-fluid rounded-5" alt="...">
+            </div>
+            <div class="col-md-8">
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional
+                  content. This content is a little bit longer.</p>
+                <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+
+      </div>
+      <div class="row">
+
+
+        <div class="card mb-3" style="max-width: auto;">
+          <div class="row g-0">
+            <div class="col-md-4">
+              <img src="../assets/dependencias/landing/dependencias/imagenes/webP/info2.webp" class="img-fluid rounded-5" alt="...">
+            </div>
+            <div class="col-md-8">
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional
+                  content. This content is a little bit longer.</p>
+                <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+
+      </div>
+    </div>
+    <div class="col-12 col-sm-12 col-md-4 ">
+
+
+      
+      <div class="card text-center mb-3 bg-success p-2 text-dark bg-opacity-50 text-white"><!-- inicio formulario -->
+        <div class="card-body">
+          <h5 class="card-title">REGISTRO AGRICULTOR</h5>
+          <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div><!-- fin formulario -->
+      
+
+
     </div>
   </div>
 </div>
